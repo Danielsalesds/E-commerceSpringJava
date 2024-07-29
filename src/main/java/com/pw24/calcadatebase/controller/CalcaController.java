dd1b4d2f6a8680ec0535398f85c7ed998d0b0508
danielsalesds <danielsales3@gmail.com>
2024-07-29 13:41:43 -0300

    tela de cadastro e funcao excluir calca

--- src/main/java/com/pw24/calcadatebase/controller/CalcaController.java
+++ src/main/java/com/pw24/calcadatebase/controller/CalcaController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,28 +29,41 @@ public class CalcaController {
     }
     @GetMapping("/")
     public String inicialPage(Model model) {
-        model.addAttribute("calca", service.findAll());
-
         return "index";
+    }
+    @GetMapping("/updatePage")
+    public String pageAtualizada(Model model){
+        model.addAttribute("calcas", service.findAll());
+        return "calcacadastro";
     }
 
     @GetMapping("/calcacadastrar")
     public String getCadastroPage(Model model) {
         Calca calca = new Calca();
         model.addAttribute("calca", calca);
+        model.addAttribute("calcas", service.findAll());
+
         return "calcacadastro";
     }
-        @PostMapping("/processCadastroPage")
-        public ModelAndView processCadastroPage(@ModelAttribute @Valid Calca calca, Errors errors, @RequestParam("calca") MultipartFile file){
-            if(errors.hasErrors()){
-                return new ModelAndView("calcacadastro");
-            }
-            service.create(calca);
-            ModelAndView modelAndView = new ModelAndView("index");
-            modelAndView.addObject("msg", "Cadastro realizado com sucesso");
-            modelAndView.addObject("calca", service.findAll());
-            return modelAndView;
+    @PostMapping("/processCadastroPage")
+    public String processCadastroPage(@ModelAttribute @Valid Calca calca, Errors errors){
+        //if(errors.hasErrors()){
+         //   return new ModelAndView("calcacadastro");
+        //}
+        calca.upperCaseCalcaName();
+        service.create(calca);
+        
+        ModelAndView modelAndView = new ModelAndView("calcacadastro");
+        modelAndView.addObject("msg", "Cadastro realizado com sucesso");
+        //modelAndView.addObject("calcas", service.findAll());
+        return "redirect:/calcacadastrar";
 
+
+    }
+    @GetMapping("/processDelete/{id}")
+    public String processDelete(@PathVariable String id){
+        service.delete(id);
+        return "redirect:/calcacadastrar";
 
     }
 
--- src/main/java/com/pw24/calcadatebase/controller/UsuarioController.java
+++ src/main/java/com/pw24/calcadatebase/controller/UsuarioController.java
@@ -1,5 +1,29 @@
 package com.pw24.calcadatebase.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.pw24.calcadatebase.domain.Usuario;
+import com.pw24.calcadatebase.service.UsuarioService;
+
+@Controller
 public class UsuarioController {
+
+    private final UsuarioService service;
+
+    public UsuarioController(UsuarioService service){
+        this.service = service;
+    }
+    @GetMapping("/usuarioCadastro")
+    public String inicialPage(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
+        return "usuariocadastro";
+    }
+
+
+
+
     
 }
